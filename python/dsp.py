Fixed critical bug with normalization filter, renamed 'decay 'to r'ise'

python/dsp.py
@@ -1,15 +1,17 @@
 from __future__ import print_function
 from __future__ import division
 import time
+import functools
 import numpy as np
 import config
 
 
 class RealTimeExpFilter:
-    """Exponential filter that works when sampling time is noisy"""
+    """Exponential filter that references time.time()
+    """
 
-    def __init__(self, val=None, decay=0.5, rise=0.5):
-        self.decay = decay
+    def __init__(self, val=None, fall=0.5, rise=0.5):
+        self.fall = fall
         self.rise = rise
         self.value = val
         self.t_prev = None
@@ -31,14 +33,14 @@ class RealTimeExpFilter:
         self.t_prev += dt
 
         def a(tau):
-            return 1.0 - np.exp(-dt / tau)
+            return 1.0 - np.exp(-dt / tau) if tau else 1.0
 
         if isinstance(self.value, (list, np.ndarray, tuple)):
             alpha = value - self.value
             alpha[alpha > 0.0] = a(self.rise)
-            alpha[alpha <= 0.0] = a(self.decay)
+            alpha[alpha <= 0.0] = a(self.fall)
         else:
-            alpha = a(self.rise) if value > self.value else a(self.decay)
+            alpha = a(self.rise) if value > self.value else a(self.fall)
 
         self.value += alpha * (value - self.value)
 
@@ -57,10 +59,10 @@ def a(tau):
 class ExpFilter:
     """Simple exponential filter"""
 
-    def __init__(self, val=None, decay=0.5, rise=0.5):
-        assert 0.0 < decay < 1.0, 'Invalid decay smoothing factor'
+    def __init__(self, val=None, fall=0.5, rise=0.5):
+        assert 0.0 < fall < 1.0, 'Invalid fall smoothing factor'
         assert 0.0 < rise <= 1.0, 'Invalid rise smoothing factor'
-        self.decay = decay
+        self.fall = fall
         self.rise = rise
         self.value = val
 
@@ -73,9 +75,9 @@ class ExpFilter:
         if isinstance(self.value, (list, np.ndarray, tuple)):
             alpha = value - self.value
             alpha[alpha > 0.0] = self.rise
-            alpha[alpha <= 0.0] = self.decay
+            alpha[alpha <= 0.0] = self.fall
         else:
-            alpha = self.rise if value > self.value else self.decay
+            alpha = self.rise if value > self.value else self.fall
 
         self.value = alpha * value + (1.0 - alpha) * self.value
         if isinstance(self.value, (list, np.ndarray, tuple)):
@@ -84,18 +86,17 @@ class ExpFilter:
             return self.value
 
 
-def ApplyNormalization(decay, rise=1, realtime=False):
+def ApplyNormalization(fall, rise, realtime=False):
     """Decorator that applies peak normalization"""
-    def normalization_decorator(function):
+    def normalization_decorator(func):
         if realtime:
-            filt = RealTimeExpFilter(decay=decay, rise=rise)
+            filt = RealTimeExpFilter(fall=fall, rise=rise)
         else:
-            filt = ExpFilter(decay=decay, rise=rise)
+            filt = ExpFilter(fall=fall, rise=rise)
         epsilon = np.finfo(float).eps
 
         def wrapper(*args, **kwargs):
-            value = function(*args, **kwargs)
-            filt.update(np.abs(value))
+            value = func(*args, **kwargs)
             if np.isscalar(value):
                 filt.update(np.abs(value))
             else:
@@ -105,27 +106,63 @@ def ApplyNormalization(decay, rise=1, realtime=False):
                 if filt.value == 0.0:
                     filt.value = epsilon
             else:
-                filt.value = np.where(filt.value == 0, epsilon, filt.value)
-            value /= filt.value
+                filt.value[filt.value == 0.0] = epsilon
+            value = value / filt.value
             return value
         return wrapper
     return normalization_decorator
 
 
-def ApplyExpFilter(decay, rise, initial_value=None, realtime=False):
+def ApplyExpFilter(fall, rise, initial_value=None, realtime=False):
     """Decorator that applies exponential smoothing"""
-    def filter_decorator(function):
+    def filter_decorator(func):
         if realtime:
-            filt = RealTimeExpFilter(decay=decay, rise=rise)
+            filt = RealTimeExpFilter(fall=fall, rise=rise)
         else:
-            filt = ExpFilter(decay=decay, rise=rise)
+            filt = ExpFilter(fall=fall, rise=rise)
         filt.value = initial_value
 
         def wrapper(*args, **kwargs):
-            filt.update(function(*args, **kwargs))
+            filt.update(func(*args, **kwargs))
             return filt.value
         return wrapper
     return filter_decorator
+
+
+def apply_filt_lr(y, filt):
+    """Applies filter to the array bidirectionally
+
+    Applies a dsp.ExpFilter to the array in both directions.
+    Applying a bidirectional filter removes the phase lag.
+
+    Parameters
+    ==========
+    y: np.array
+        Array to apply the filter to. This is not modified.
+    filt: dsp.ExpFilter
+        Filter to apply to the array
+
+    Returns
+    =======
+    out: np.array
+        Returns a new array which is the combined result of the left and
+        right applications of the dsp.ExpFilter
+    """
+    if np.isnan(y).any():
+        raise ValueError('Cannot operate on array containing NaN value(s)')
+    L = np.zeros_like(y)
+    R = np.zeros_like(y)
+    # Apply filter moving rightwards
+    filt.value = y[0]
+    for i in range(len(y)):
+        R[i] = filt.update(y[i])
+    filt.value = y[-1]
+    # Apply filter moving leftwards
+    for i in range(len(y) - 1, 0, -1):
+        L[i] = filt.update(y[i])
+    # Combine results
+    return (L**2.0 + R**2.0)**0.5
+    #return (L + R) / 2.0
 
 
 def preemphasis(signal, coeff=0.97):
@@ -219,32 +256,26 @@ if __name__ == '__main__':
     import matplotlib.pylab as plt
     config.FPS = 100
 
-    def a(tau):
-        """Converts time constant into the corresponding ExpFilter alpha value"""
-        dT = 1.0 / config.FPS
+    def a(tau, dT=1):
         return 1.0 - np.exp(-dT / tau)
 
-    N = 10.0 * config.FPS
-    t_clean = np.linspace(0, N / config.FPS, N)
-    noise = np.random.random(N) - 0.5
-    noise *= t_clean[1] - t_clean[0]
-    noise /= 100.0
-    t_noisy = t_clean + noise
+    def foo(samples, lp):
+        # lp = ExpFilter(rise=a(0.1), fall=a(3))
+        x = np.linspace(0, 10, samples)
+        y = (np.sin(3.0 * x) ** 4.0 + np.sin(2.0 * x) ** 2.0) / 2.0
+        z = apply_filt_lr(y, lp)
+        return x, y, z
+    
+    a1 = 1.0 - np.exp(-256 / (3 * 256.0))
+    a2 = 1.0 - np.exp(-256 / (3 * 1024.0))
 
-    x = np.sin(300. * 2. * np.pi * t_clean)
-    y = np.sin(300. * 2. * np.pi * t_noisy)
-    z = np.sin(300. * 2. * np.pi * t_noisy)
+    lp = ExpFilter(rise=a1, fall=a1)
+    x1, y1, z1 = foo(256, lp)
+    lp = ExpFilter(rise=a2, fall=a2)
+    x2, y2, z2 = foo(1024, lp)
 
-    a = ExpFilter(decay=a(0.5), rise=a(0.5))
-    b = ExpFilter(decay=0.5, rise=0.5)
-    c = RealTimeExpFilter(decay=0.5, rise=0.5)
-
-    for i in range(len(y)):
-        x[i] = a.update(x[i])
-        y[i] = b.update(y[i])
-        z[i] = c.update(z[i], t=t_noisy[i])
-
-    plt.plot(t_clean, x, color='red')
-    plt.plot(t_noisy, y, color='green')
-    plt.plot(t_noisy, z, color='blue')
+    plt.plot(x1, z1, label='y1', linewidth=3)
+    plt.plot(x2, z2, label='y2', linewidth=3)
+    plt.legend().draggable()
     plt.show()
+

python/features.py
@@ -7,20 +7,20 @@ import config
 import dsp
 
 
-@dsp.ApplyNormalization(decay=3, rise=1e-4, realtime=True)
+@dsp.ApplyNormalization(fall=3, rise=1e-4, realtime=True)
 def zero_crossing_rate(audio):
     """Returns the number of zero crossings in the array"""
     zcr = ((audio[:-1] * audio[1:]) < 0).sum() / len(audio)
     return zcr
 
 
-@dsp.ApplyNormalization(decay=1, rise=1e-4, realtime=True)
+@dsp.ApplyNormalization(fall=1, rise=1e-4, realtime=True)
 def mel_spectrum(y):
     N = len(y)
     # Pre-emphasis filter to amplify high frequencies
-    # y = dsp.preemphasis(y, coeff=0.8)
+    y = dsp.preemphasis(y, coeff=0.5)
     # Apply Hamming window to reduce spectral leakage
-    y *= np.hamming(N)
+    y = y * np.hamming(N)
     # Transform to frequency domain
     mag_spectrum = np.absolute(np.fft.rfft(y))
     pow_spectrum = (mag_spectrum**2.0) / N

python/led.py
@@ -6,7 +6,7 @@ import numpy as np
 import config
 
 
-_gamma = np.load(config.GAMMA_TABLE_PATH)
+_gamma = np.genfromtxt(config.GAMMA_TABLE_PATH, dtype=int, delimiter=' ')
 """Gamma lookup table used for nonlinear brightness correction"""
 
 _prev_pixels = np.tile(253, (3, config.N_PIXELS))
@@ -75,7 +75,7 @@ def _update_pi():
     """
     global pixels, _prev_pixels
     # Truncate values and cast to integer
-    pixels = np.clip(pixels, 0, 255).astype(long)
+    pixels = np.clip(pixels, 0, 255).astype(int)
     # Optional gamma correction
     p = _gamma[pixels] if config.SOFTWARE_GAMMA_CORRECTION else np.copy(pixels)
     # Encode 24-bit LED values in 32 bit integers
@@ -105,11 +105,12 @@ def update():
 
 # Execute this file to run a LED strand test
 # If everything is working, you should see a red, green, and blue pixel scroll
-# across the LED strip continously
+# across the LED strip continously 
 if __name__ == '__main__':
     import time
     # Turn all pixels off
     pixels *= 0
+    update()
     pixels[0, 0] = 255  # Set 1st pixel red
     pixels[1, 1] = 255  # Set 2nd pixel green
     pixels[2, 2] = 255  # Set 3rd pixel blue
@@ -117,4 +118,4 @@ if __name__ == '__main__':
     while True:
         pixels = np.roll(pixels, 1, axis=1)
         update()
-        time.sleep(1)
+        time.sleep(.1)

python/main.py
@@ -5,34 +5,32 @@ from __future__ import print_function
 import time
 import imp
 import numpy as np
-import pyaudio
+from scipy import signal
 import config
 import led
 import dsp
 import features
 import visualize
-import microphone
+import audio
 if config.USE_GUI:
     from pyqtgraph.Qt import QtGui
-    import gui2
-
-effects = {
-    'Spectrum': visualize.spectrum,
-    'Scroll': visualize.scroll,
-    'Energy': visualize.energy
-}
+    import gui
 
 current_effect = 'Spectrum'
 """Currently selected visualization effect"""
 
-prev_audio = np.array([0.0])
-"""Previous audio frames used to construct a rolling window"""
+prev_frames = np.array([0.0])
+"""Contains the previous batch of audio frames"""
 
-using_stream_flag = False
-"""Flag that tells the GUI thread when it isn't safe to close the stream"""
+rolling_window = np.array([0.0])
+"""Contains rolling audio window frames"""
 
-rolling_window = np.array([0])
-"""Rolling audio window"""
+gui_settings_dict = {}
+"""Dictionary containing the values of the GUI setting controls"""
+
+gui_settings_changed_flag = False
+"""Flag for indicating that GUI settings have been changed"""
+
 
 p1c1_y = np.arange(0, 1)
 p1c2_y = np.arange(0, 1)
@@ -45,7 +43,7 @@ p2c3_y = np.arange(0, 1)
 
 def terminate():
     """Shut things down properly before exiting"""
-    microphone.close_stream()
+    audio.end_stream()
     led.pixels *= 0
     led.update()
     raise SystemExit
@@ -56,37 +54,39 @@ def init_gui_plots():
     global p2c1, p2c2, p2c3
     # Plot 1 Configuration
     win.plot1.setRange(yRange=[-5, 260])
-    win.plot1.disableAutoRange(axis=gui2.pg.ViewBox.YAxis)
-    r = gui2.pg.mkPen((255, 30, 30, 200), width=4)
-    g = gui2.pg.mkPen((30, 255, 30, 200), width=4)
-    b = gui2.pg.mkPen((30, 30, 255, 200), width=4)
-    p1c1 = gui2.pg.PlotCurveItem(pen=r)
-    p1c2 = gui2.pg.PlotCurveItem(pen=g)
-    p1c3 = gui2.pg.PlotCurveItem(pen=b)
+    win.plot1.disableAutoRange(axis=gui.pg.ViewBox.YAxis)
+    r = gui.pg.mkPen((255, 30, 30, 200), width=4)
+    g = gui.pg.mkPen((30, 255, 30, 200), width=4)
+    b = gui.pg.mkPen((30, 30, 255, 200), width=4)
+    p1c1 = gui.pg.PlotCurveItem(pen=r)
+    p1c2 = gui.pg.PlotCurveItem(pen=g)
+    p1c3 = gui.pg.PlotCurveItem(pen=b)
     win.plot1.addItem(p1c1)
     win.plot1.addItem(p1c2)
     win.plot1.addItem(p1c3)
     # Plot 2 Configuration
     win.plot2.setRange(yRange=[-5, 260])
-    win.plot2.disableAutoRange(axis=gui2.pg.ViewBox.YAxis)
-    r = gui2.pg.mkPen((255, 30, 30, 200), width=4)
-    g = gui2.pg.mkPen((30, 255, 30, 200), width=4)
-    b = gui2.pg.mkPen((30, 30, 255, 200), width=4)
-    p2c1 = gui2.pg.PlotCurveItem(pen=r)
-    p2c2 = gui2.pg.PlotCurveItem(pen=g)
-    p2c3 = gui2.pg.PlotCurveItem(pen=b)
+    win.plot2.disableAutoRange(axis=gui.pg.ViewBox.YAxis)
+    r = gui.pg.mkPen((255, 30, 30, 200), width=4)
+    g = gui.pg.mkPen((30, 255, 30, 200), width=4)
+    b = gui.pg.mkPen((30, 30, 255, 200), width=4)
+    p2c1 = gui.pg.PlotCurveItem(pen=r)
+    p2c2 = gui.pg.PlotCurveItem(pen=g)
+    p2c3 = gui.pg.PlotCurveItem(pen=b)
+
     win.plot2.addItem(p2c1)
     win.plot2.addItem(p2c2)
     win.plot2.addItem(p2c3)
 
 
-def audio_visualization(audio):
+def audio_visualization(audio_frames):
     global p1c1_y, p1c2_y, p1c3_y
     global p2c1_y, p2c2_y, p2c3_y
-    """Maps audio input to an LED strip visualization"""
-    audio = audio / 2.0**15
-    filter_bank_output = features.mel_spectrum(audio)
-    led_output = effects[current_effect](filter_bank_output)
+    """Maps audio_frames input to an LED strip visualization"""
+    audio_frames = audio_frames / 2.0**15
+    freq_data = features.mel_spectrum(audio_frames)
+    effect_function = getattr(visualize, current_effect.lower())
+    led_output = effect_function(freq_data, audio_frames) * 255
     led.pixels = led_output
     led.update()
 
@@ -95,49 +95,55 @@ def audio_visualization(audio):
     p2c3_y = led_output[2, :]
 
 
-def callback(in_data, frame_count, time_info, flag):
-    global using_stream_flag
-    global prev_audio
+def update_rolling_window():
+    global prev_frames
     global rolling_window
-    using_stream_flag = True
+    frame_count = config.MIC_RATE // config.FPS
+    frames_available = audio.stream.get_read_available()
+    # We only want frames that are available immediately
+    if frames_available >= frame_count:
+        frames = np.fromstring(audio.stream.read(frame_count), np.float32)
+        frames = signal.detrend(frames)
+        rolling_window = np.concatenate((prev_frames, frames))
+        prev_frames = frames[frame_count // 2:]
 
-    # Rolling audio window
-    audio = np.fromstring(in_data, np.float32)
-    rolling_window = np.concatenate((prev_audio, audio))
-    prev_audio = audio[frame_count // 2:]
 
-    using_stream_flag = False
-    return (audio, pyaudio.paContinue)
-
-
-def settings_updated(settings_dict):
-    """Update the settings because the user changed a setting in the GUI"""
+def update_settings():
     global current_effect
-    current_effect = settings_dict['effect']
-    while using_stream_flag:
-        pass
-    microphone.close_stream()
-
-    # Update config with new settings
-    config.set_config_from_dict(settings_dict)
+    # Update settings values
+    current_effect = gui_settings_dict['effect']
+    config.set_config_from_dict(gui_settings_dict)
     config.write()
-    # Reload modules to prevent errors
-    # It's hacky but it works
-
+    # Reload imports to reinitialize with updated settings values
+    # Feels a bit hacky but it works.
     imp.reload(led)
     imp.reload(dsp)
     imp.reload(features)
     imp.reload(visualize)
-    imp.reload(visualize)
-    # Restart the audio stream
-    microphone.start_stream(callback=callback)
-    print('Settings changed. Audio stream has been restarted.')
+    if not gui_settings_dict['show_plot']:
+        for widget in win.plot1.children():
+            if isinstance(widget, gui.pg.PlotCurveItem):
+                widget.clear()
+        for widget in win.plot2.children():
+            if isinstance(widget, gui.pg.PlotCurveItem):
+                widget.clear()
+        app.processEvents()
+
+    print('Settings changed')
+
+
+def guiSettingsChanged(settings_dict):
+    """Update the settings because the user changed a setting in the GUI"""
+    global gui_settings_dict
+    global gui_settings_changed_flag
+    gui_settings_dict = settings_dict
+    gui_settings_changed_flag = True
 
 
 if __name__ == '__main__':
-    # Create the GUI window
+    # Create and show the GUI
     if config.USE_GUI:
-        effect_index = list(effects.keys()).index(current_effect)
+        effect_index = 0  # list(effects.keys()).index(current_effect)
         initial_settings = {
             'fps': config.FPS,
             'pixels': config.N_PIXELS,
@@ -150,35 +156,40 @@ if __name__ == '__main__':
         }
         app = QtGui.QApplication([])
 
-        win = gui2.MainWindow(initial_settings)
-        win.settingsUpdated.connect(settings_updated)
+        win = gui.MainWindow(initial_settings)
+        win.settingsUpdated.connect(guiSettingsChanged)
         win.closing.connect(terminate)
         win.settingsChanged(0)
         init_gui_plots()
         win.show()
 
-    t = time.time()
 
-    microphone.start_stream(callback=callback)
+    fps_filter = dsp.RealTimeExpFilter(config.FPS, rise=0.25, fall=0.25)
+    t = time.time()
+    audio.start_stream()
+
     while True:
-        # Visualization and output
+        if config.USE_GUI:
+            # FPS counter
+            fps = fps_filter.update(1. / (time.time() - t))
+            t = time.time()
+            win.fpsLabel.setText('FPS: {:.0f}'.format(fps))
+            if gui_settings_changed_flag:
+                gui_settings_changed_flag = False
+                audio.end_stream()
+                update_settings()
+                audio.start_stream()
+        update_rolling_window()
         audio_visualization(rolling_window)
 
-        #x1 = np.arange(len(p1c1_y))
-        #x2 = np.arange(len(p2c1_y))
+        if gui_settings_dict['show_plot']:
+            x = np.arange(len(p2c1_y))
+            p2c1.setData(x=x, y=p2c1_y)
+            p2c2.setData(x=x, y=p2c2_y)
+            p2c3.setData(x=x, y=p2c3_y)
 
-        # p1c1.setData(x=x1, y=p1c1_y)
-        # p1c2.setData(x=x1, y=p1c2_y)
-        # p1c3.setData(x=x1, y=p1c3_y)
 
-        # p2c1.setData(x=x2, y=p2c1_y)
-        # p2c2.setData(x=x2, y=p2c2_y)
-        # p2c3.setData(x=x2, y=p2c3_y)
 
-        print(1 / (time.time() - t))
-        t = time.time()
-
-        #time.sleep(0.00)
+        time.sleep(1e-3) # For numerical stability
         app.processEvents()
-        
-    microphone.close_stream()
+    audio.end_stream()

python/visualize.py
@@ -2,8 +2,8 @@ from __future__ import print_function
 from __future__ import division
 import time
 import numpy as np
-from scipy.ndimage.filters import gaussian_filter1d
 import config
+import features
 import dsp
 
 scroll_pixels = np.tile(0.1, config.N_PIXELS // 2)
@@ -72,54 +72,92 @@ def interpolate(y, new_length):
     return z
 
 
-@dsp.ApplyExpFilter(decay=0.05, rise=1e-3, realtime=True)
-def scroll(y):
+scroll_time = time.time()
+
+
+@dsp.ApplyExpFilter(fall=0.01, rise=1e-4, realtime=True)
+def scroll(f, t, pixels_per_sec=60.0):
     """Effect that originates in the center and scrolls outwards"""
     global scroll_pixels
+    global scroll_time
     # Shift LED strip
-    scroll_pixels = np.roll(scroll_pixels, 1)
-    scroll_pixels *= 0.98
+    if time.time() - scroll_time > 1 / pixels_per_sec:
+        scroll_time = time.time()
+        scroll_pixels = np.roll(scroll_pixels, 1)
+        # fall constant
+        scroll_pixels *= np.exp(-2.0 / config.N_PIXELS)
     # Calculate brightness of origin
-    brightness = np.max(y)**4.0
+    brightness = np.max(f)**4.0
     # Create new color originating at the center
     scroll_pixels[0] = brightness
-    scroll_pixels = gaussian_filter1d(scroll_pixels, sigma=0.1)
-    output = rainbow(config.N_PIXELS, speed=1.0 / 5.0) * 255
+    output = rainbow(config.N_PIXELS, speed=1.0 / 5.0)
     output *= np.concatenate((scroll_pixels[::-1], scroll_pixels))
     return output
 
 
-energy_filter = dsp.ExpFilter(decay=0.2, rise=1)
+rms_energy = dsp.RealTimeExpFilter(fall=3, rise=1e-3)
 
 
-@dsp.ApplyExpFilter(decay=0.5, rise=0.5, realtime=True)
-def energy(y):
-    """Effect that expands from the center with increasing sound energy"""
-    p = np.tile(0, (3, config.N_PIXELS // 2))
-    y = y * float((config.N_PIXELS // 2) - 1)
-    for i in range(len(y)):
-        y[i] = energy_filter.update(float(y[i]))
-    # Map color channels according to energy in the different freq bands
-    scale = 0.9
-    r = np.round(np.mean(y[:len(y) // 3]**scale))
-    g = np.round(np.mean(y[len(y) // 3: 2 * len(y) // 3]**scale))
-    b = np.round(np.mean(y[2 * len(y) // 3:]**scale))
-    # Assign color to different frequency regions
-    p[0, :int(r)] = 255.0
-    p[1, :int(g)] = 255.0
-    p[2, :int(b)] = 255.0
-    # Apply substantial blur to smooth the edges
-    p[0, :] = gaussian_filter1d(p[0, :], sigma=3.0)
-    p[1, :] = gaussian_filter1d(p[1, :], sigma=3.0)
-    p[2, :] = gaussian_filter1d(p[2, :], sigma=3.0)
-    # Set the new pixel value
-    return np.concatenate((p[:, ::-1], p), axis=1)
+@dsp.ApplyExpFilter(fall=0.05, rise=0.01, realtime=True)
+def energy(f, t):
+    p = np.zeros((3, config.N_PIXELS // 2))
+    rms = np.sqrt(np.mean(np.square(f)))
+    p[:, 0] = rms / rms_energy.update(rms)
+    for i in range(1, config.N_PIXELS // 2):
+        p[:, i] = p[:, i - 1] * np.exp(-2.0 / config.N_PIXELS)**4
+    shift = config.N_PIXELS // 4
+    output = np.concatenate((p[:, ::-1], p), axis=1)
+    output[1] = np.roll(output[1], shift)
+    output[2] = np.roll(output[2], -shift)
+    return output
 
 
-@dsp.ApplyExpFilter(decay=0.05, rise=0.001, realtime=True)
-def spectrum(y):
+# Alpha values for spectrum effect low pass exponential filter
+a_rise = 1.0 - np.exp(-60.0 / (0.1 * config.N_PIXELS))
+a_fall = 1.0 - np.exp(-60.0 / (4.0 * config.N_PIXELS))
+lp = dsp.ExpFilter(rise=a_rise, fall=a_fall)
+
+
+@dsp.ApplyExpFilter(fall=0.15, rise=0.001, realtime=True)
+def spectrum(f, t, threshold=0.0):
     """Effect that maps the filterbank frequencies onto the LED strip"""
-    y = np.copy(interpolate(y**0.5, config.N_PIXELS // 2))
+    # y = np.copy(interpolate(y**0.5, config.N_PIXELS // 2))
+    f = np.copy(interpolate(f**1.5, config.N_PIXELS // 2))
+    f = np.concatenate((f[::-1], f))
+    f = dsp.apply_filt_lr(f, lp)
+    f[f <= threshold] = 0.0
     output = rainbow(config.N_PIXELS)
-    output *= np.concatenate((y[::-1], y))
-    return output * 255.0
+    output *= f
+    return output
+
+
+zcr_filter = dsp.RealTimeExpFilter(rise=0.0, fall=1e-4)
+"""Smooths the zero-crossing rate"""
+particle_time = time.time()
+particle_location = 0.0
+particle_friction_mu = 0.1
+particle_mass = 0.0
+particle_energy = 0.0
+particle_velocity = dsp.RealTimeExpFilter(rise=0.0, fall=1)
+
+
+#@dsp.ApplyNormalization(rise=0.0, fall=2.0, realtime=True)
+@dsp.ApplyExpFilter(fall=0.1, rise=0.0, realtime=True)
+def particle(f, t, pixels_per_sec=60.0):
+    global particle_time
+    global particle_location
+    dt = time.time() - particle_time
+    particle_time += dt
+    zcr = features.zero_crossing_rate(t)
+    velocity = particle_velocity.update(max(0, zcr - 0.5))
+    print(velocity)
+    particle_location += dt * pixels_per_sec * velocity*0.2
+    color = rainbow(config.N_PIXELS)
+    output = np.zeros(config.N_PIXELS)
+    output[int(round(particle_location)) % config.N_PIXELS] = zcr
+    return output * color
+
+    
+
+
+