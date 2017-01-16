b80fbbbebce0ec9ec7478ae1ecf29fe0282eabce
Scott Lawson <scottlawsonbc@gmail.com>
2017-01-16 00:24:43 -0800

    GUI overhaul
    Significant overhaul of the GUI. The settings panel is now located at
    the bottom. A spectrogram is now displayed instead of a line plot.

--- python/gui.py
+++ python/gui.py
@@ -17,9 +17,6 @@ class MainWindow(QtGui.QMainWindow):
     closing = QtCore.pyqtSignal()
     """Emitted when closeEvent is emitted"""
 
-    isFullscreen = False
-    """Current window state"""
-
     def __init__(self, settings_dict=None):
         super(MainWindow, self).__init__()
         uic.loadUi(config.GUI_UI_FILE_PATH, self)
@@ -30,15 +27,34 @@ class MainWindow(QtGui.QMainWindow):
         # No point having everything fail because we couldn't set an icon
         try:
             icon = QtGui.QIcon()
-            icon.addFile('icons/48x48.png', QtCore.QSize(48, 48))
+            icon.addFile('icons/256x256.png', QtCore.QSize(256, 256))
             self.setWindowIcon(icon)
         except IOError as e:
             print('Error setting application icon:', e)
+        # Add curves to plot
+        self.initPlots()
         # Hotkeys for convenience
         QtGui.QShortcut(QtGui.QKeySequence("Ctrl+W"), self, self.close)
         QtGui.QShortcut(QtGui.QKeySequence("Esc"), self, self.close)
-        QtGui.QShortcut(QtGui.QKeySequence("F11"), self, self.fullscreenToggled)
-        QtGui.QShortcut(QtGui.QKeySequence("f"), self, self.fullscreenToggled)
+        QtGui.QShortcut(QtGui.QKeySequence("h"), self, self.settingsToggled)
+
+    def initPlots(self):
+        self.plot1. setRange(yRange=[-0.1, 1.1])
+        self.plot1.disableAutoRange(axis=pg.ViewBox.YAxis)
+        r = pg.mkPen((255, 30, 30, 200), width=4)
+        g = pg.mkPen((30, 255, 30, 200), width=4)
+        b = pg.mkPen((30, 30, 255, 200), width=4)
+        w = pg.mkPen((255, 255, 255, 200), width=1)
+        colors = [r, g, b, w]
+        curves = [pg.PlotCurveItem(pen=c) for c in colors]
+        for curve in curves:
+            self.plot1.addItem(curve)
+        self.plot1.curves = curves
+
+        self.img = pg.ImageItem()
+        self.vb = self.plot2.addViewBox(row=1, col=1)
+        self.vb.setContentsMargins(0, 0, 0, 0)
+        self.vb.addItem(self.img)
 
     def closeEvent(self, event):
         self.closing.emit()
@@ -54,8 +70,9 @@ class MainWindow(QtGui.QMainWindow):
         self.minFreq.setValue(settings_dict['min_freq'])
         self.maxFreq.setValue(settings_dict['max_freq'])
         self.fftBins.setValue(settings_dict['fft_bins'])
+        self.colormap.setCurrentIndex(settings_dict['cmap_index'])
 
-    def settingsChanged(self, val):
+    def settingsChanged(self, val=None):
         """Slot called when a GUI setting has been changed"""
         settings = {
             'fps': self.fps.value(),
@@ -66,23 +83,29 @@ class MainWindow(QtGui.QMainWindow):
             'min_freq': self.minFreq.value(),
             'max_freq': self.maxFreq.value(),
             'fft_bins': self.fftBins.value(),
-            'show_plot': self.showPlot.isChecked()
-
+            'show_plot': self.showPlot.isChecked(),
+            'cmap': str(self.colormap.currentText())
         }
         self.settingsUpdated.emit(settings)
 
-    def fullscreenToggled(self):
-        """Toggles fullscreen mode on or off"""
-        self.isFullscreen = not self.isFullscreen
-        if self.isFullscreen:
-            self.showFullScreen()
+    def showPlotToggled(self, show_plot):
+        # self.plot1.show() if show_plot else self.plot1.hide()
+        self.plotSplitter.show() if show_plot else self.plotSplitter.hide()
+        self.resize(self.sizeHint())
+
+    def settingsToggled(self):
+        if self.settingsFrame.isVisible():
+            self.settingsFrame.hide()
         else:
-            self.showNormal()
+            self.settingsFrame.show()
+
+    def fullscreenToggled(self, isFullscreen):
+        """Toggles fullscreen mode on or off"""
+        self.showFullScreen() if isFullscreen else self.showNormal()
 
 
 if __name__ == '__main__':
     app = QtGui.QApplication([])
     win = MainWindow()
     win.show()
-    while True:
-        app.processEvents()
+    app.exec_()
