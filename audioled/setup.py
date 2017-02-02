f719049d082844295f6880f616948dc5b0bfbf5a
Scott Lawson <scottlawsonbc@gmail.com>
2017-02-02 03:52:56 -0800

    Remove future imports in setup.py

--- audioled/setup.py
+++ audioled/setup.py
@@ -1,8 +1,4 @@
 #! /usr/bin/env python
-from __future__ import print_function
-from __future__ import unicode_literals
-from __future__ import absolute_import
-from __future__ import division
 from setuptools import setup
 
 setup(name='audioled',
