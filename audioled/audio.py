127a431ebf4e9535e7b92fcfb8748fc61243c04a
Scott Lawson <scottlawsonbc@gmail.com>
2017-02-02 03:44:16 -0800

    Add device index option, audio device utility
    Add function that prints information about the system's audio devices.
    Add ability to manually override the default device and use a specific
    audio device using the provided device index. This option is useful for
    raspberry pi users and windows users using Stereo Mix.

--- audioled/audio.py
+++ audioled/audio.py
@@ -6,63 +6,79 @@ import numpy as np
 import pyaudio
 
 
-def default_input_stream():
-    """Opens a PyAudio stream using the default audio input device"""
+def _open_input_stream(device_index=None):
+    """Opens a PyAudio audio input stream
+
+    Parameters
+    ----------
+    device_index: int, optional
+        Device index for the PyAudio audio input stream.
+        If device index is not specified then the default audio device
+        will be opened.
+    """
     p = pyaudio.PyAudio()
     defaults = p.get_default_host_api_info()
-    default_index = defaults['defaultInputDevice']
-    default_input = p.get_device_info_by_index(default_index)
+    if device_index is None:
+        device_index = defaults['defaultInputDevice']
+    device_info = p.get_device_info_by_index(device_index)
 
-    if default_input['maxInputChannels'] == 0:
-        err = 'Your default audio input device cannot be opened. '
-        err += 'Change your system default audio input device and try again. '
-        err += 'Device info:\n{}\n{}'.format(defaults, default_input)
+    if device_info['maxInputChannels'] == 0:
+        err = 'Your audio input device cannot be opened. '
+        err += 'Change default audio device or try a different device index. '
+        err += 'Device info:\n{}\n{}'.format(defaults, device_info)
         raise OSError(err)
 
     try:
         stream = p.open(format=pyaudio.paFloat32,
                         channels=1,
-                        rate=int(default_input['defaultSampleRate']),
+                        rate=int(device_info['defaultSampleRate']),
                         input=True,
+                        input_device_index=device_index,
                         frames_per_buffer=0)
     except OSError as e:
         err = 'Error occurred while attempting to open audio device. '
         err += 'Check your operating system\'s audio device configuration. '
         err += 'Audio device information: \n'
-        err += str(default_input)
+        err += str(device_info)
         print(err)
         raise e
-    return stream, int(default_input['defaultSampleRate'])
+    return stream, int(device_info['defaultSampleRate'])
 
 
-def stream_audio(chunk_rate=60, exception_on_overflow=False):
-    stream, fs = default_input_stream()
-    chunk_length = int(fs // chunk_rate)
+def stream_audio(chunk_rate=60, ignore_overflows=True, device_index=None):
+    audio_stream, samplerate = _open_input_stream(device_index)
+    chunk_length = int(samplerate // chunk_rate)
 
     def audio_chunks():
-        audio_stream = stream
-        audio_fs = fs
+        stream = audio_stream
+        fs = samplerate
         while True:
             try:
-                chunk = audio_stream.read(chunk_length)
+                chunk = stream.read(chunk_length)
             except OSError as e:
                 if e.errno == pyaudio.paInputOverflowed:
                     msg = 'The audio input buffer has overflowed. '
                     msg = 'An occasional overflow is normal. '
                     msg += 'Try lowering the FPS if this happens often.'
                     print(msg)
-                    if exception_on_overflow:
+                    if not ignore_overflows:
                         raise e
                     else:
-                        audio_stream, audio_fs = default_input_stream()
+                        stream, fs = _open_input_stream(device_index)
                         chunk = stream.read(chunk_length)
             chunk = np.fromstring(chunk, np.float32)
             chunk = chunk.astype(np.float64)
             yield chunk
-    return audio_chunks(), fs
+    return audio_chunks(), samplerate
 
-if __name__ == '__main__':
-    # List all audio devices
+
+def print_audio_devices():
+    """Print information about the system's audio devices"""
     p = pyaudio.PyAudio()
     for i in range(p.get_device_count()):
-        print(p.get_device_info_by_index(i))
+        info = p.get_device_info_by_index(i)
+        print(info['name'])
+        print('\tDevice index:', info['index'])
+        print('\tSample rate:', info['defaultSampleRate'])
+        print('\tMax input channels:', info['maxInputChannels'])
+        print('\tMax output channels:', info['maxOutputChannels'])
