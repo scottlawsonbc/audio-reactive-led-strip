f1ddec2bc317c15d5fe4ea413093c5d1e56ad9c5
Nazmus Nasir <nazmus.nasir@gmail.com>
2018-06-29 10:08:49 -0500

    changed config.py to 50fps

--- python/config.py
+++ python/config.py
@@ -45,10 +45,10 @@ if DEVICE == 'blinkstick':
 USE_GUI = False
 """Whether or not to display a PyQtGraph GUI plot of visualization"""
 
-DISPLAY_FPS = False
+DISPLAY_FPS = True
 """Whether to display the FPS when running (can reduce performance)"""
 
-N_PIXELS = 144
+N_PIXELS = 142
 """Number of pixels in the LED strip (must match ESP8266 firmware)"""
 
 GAMMA_TABLE_PATH = os.path.join(os.path.dirname(__file__), 'gamma_table.npy')
@@ -57,7 +57,7 @@ GAMMA_TABLE_PATH = os.path.join(os.path.dirname(__file__), 'gamma_table.npy')
 MIC_RATE = 48000
 """Sampling frequency of the microphone in Hz"""
 
-FPS = 90
+FPS = 50
 """Desired refresh rate of the visualization (frames per second)
 
 FPS indicates the desired refresh rate, or frames-per-second, of the audio
