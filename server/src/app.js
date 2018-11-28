d84ad049a4a896d8813ae5341fd74f5efe579e3c
haldersbuschd <sebastian.halder@ba-audiolabs.com>
2018-11-28 17:35:35 +0100

    Connect visjs graph to existing REST endpoints

--- server/src/app.js
+++ server/src/app.js
@@ -1,33 +1,132 @@
 import { DataSet, Network } from 'vis/index-network';
 import 'vis/dist/vis-network.min.css';
 
-// create an array with nodes
-var nodes = new DataSet([
-  {id: 1, label: 'Node 1'},
-  {id: 2, label: 'Node 2'},
-  {id: 3, label: 'Node 3'},
-  {id: 4, label: 'Node 4'},
-  {id: 5, label: 'Node 5'}
-]);
+var nodes, edges, data, options, network;
 
-// create an array with edges
-var edges = new DataSet([
-  {from: 1, to: 3},
-  {from: 1, to: 2},
-  {from: 2, to: 4},
-  {from: 2, to: 5},
-  {from: 3, to: 3}
-]);
+const createNodesFromBackend = async() => {
+  const response = await fetch('./nodes');
+  const json = response.json();
+  json.then(values => values.forEach(element => {
+    addNode(element);
+  }));
+}
 
-// create a network
-var container = document.getElementById('network');
-var data = {
-  nodes: nodes,
-  edges: edges
-};
-var options = {
-  interaction: {
-    navigationButtons: true
-  }
-};
-var network = new Network(container, data, options);
+const createEdgesFromBackend = async() => {
+  const response = await fetch('./connections');
+  const json = response.json();
+  json.then(values => values.forEach(element => {
+    addConnection(element);
+  }));
+}
+
+function addNode(node) {
+  var uid = node["py/state"]["uid"];
+  var name = node["py/state"]["effect"]["py/object"];
+  nodes.add({id: uid, label: name, shape: 'box'});
+}
+
+function addConnection(con) {
+  var state = con["py/state"];
+  console.log(state);
+  edges.add({from: state["from_node_uid"], from_channel: state["from_node_channel"], to: state["to_node_uid"], to_channel: state["to_node_channel"], arrows:'to'});
+}
+
+function createNetwork() {
+  // create an array with nodes
+  nodes = new DataSet();
+
+  // create an array with edges
+  edges = new DataSet();
+
+  // create a network
+  var container = document.getElementById('network');
+  data = {
+    nodes: nodes,
+    edges: edges
+  };
+  options = {
+    interaction: {
+      navigationButtons: false,
+      hover:true
+    },
+    manipulation: {
+      enabled: true,
+      addNode: function (data, callback) {
+        // filling in the popup DOM elements
+        document.getElementById('node-operation').innerHTML = "Add Node";
+        editNode(data, clearNodePopUp, callback);
+      },
+      addEdge: function (data, callback) {
+        if (data.from == data.to) {
+          var r = confirm("Do you want to connect the node to itself?");
+          if (r != true) {
+            callback(null);
+            return;
+          }
+        }
+        document.getElementById('edge-operation').innerHTML = "Add Edge";
+        editEdgeWithoutDrag(data, callback);
+      },
+    }
+  };
+  network = new Network(container, data, options);
+}
+
+function editNode(data, cancelAction, callback) {
+  document.getElementById('node-label').value = data.label;
+  document.getElementById('node-saveButton').onclick = saveNodeData.bind(this, data, callback);
+  document.getElementById('node-cancelButton').onclick = cancelAction.bind(this, callback);
+  document.getElementById('node-popUp').style.display = 'block';
+}
+
+function saveNodeData(data, callback) {
+  data.label = document.getElementById('node-label').value;
+  // TODO: Save node in backend
+  // TODO: Error handling
+  // TODO: Apply default style
+  console.log(data);
+  clearNodePopUp();
+  callback(data);
+}
+
+function cancelNodeEdit(callback) {
+  clearNodePopUp();
+  callback(null);
+}
+
+function clearNodePopUp() {
+  document.getElementById('node-saveButton').onclick = null;
+  document.getElementById('node-cancelButton').onclick = null;
+  document.getElementById('node-popUp').style.display = 'none';
+}
+
+function editEdgeWithoutDrag(data, callback) {
+  // filling in the popup DOM elements
+  document.getElementById('edge-label').value = data.label;
+  document.getElementById('edge-saveButton').onclick = saveEdgeData.bind(this, data, callback);
+  document.getElementById('edge-cancelButton').onclick = cancelEdgeEdit.bind(this,callback);
+  document.getElementById('edge-popUp').style.display = 'block';
+}
+
+function clearEdgePopUp() {
+  document.getElementById('edge-saveButton').onclick = null;
+  document.getElementById('edge-cancelButton').onclick = null;
+  document.getElementById('edge-popUp').style.display = 'none';
+}
+function cancelEdgeEdit(callback) {
+  clearEdgePopUp();
+  callback(null);
+}
+function saveEdgeData(data, callback) {
+  if (typeof data.to === 'object')
+    data.to = data.to.id
+  if (typeof data.from === 'object')
+    data.from = data.from.id
+  data.label = document.getElementById('edge-label').value;
+  clearEdgePopUp();
+  callback(data);
+}
+
+createNetwork();
+createNodesFromBackend();
+createEdgesFromBackend();
