aca8c7d267b784b24532ba98af1ce03deaf8e428
haldersbuschd <sebastian.halder@ba-audiolabs.com>
2018-12-12 22:00:40 +0100

    Implement AutoGain for AudioInput
    By enabling autogain the gain on the input audio can be automatically adjusted over a defined period of time up to a max gain value. This increases usability of the effects when working with unmastered audio.

--- audioled/audio.py
+++ audioled/audio.py
@@ -96,16 +96,27 @@ class AudioInput(Effect):
     1: Audio Channel 1...
     
     """
-    def __init__(self, device_index=None, chunk_rate=60, num_channels = 2):
+    def __init__(self, device_index=None, chunk_rate=60, num_channels = 2, autogain_max = 10.0, autogain = False, autogain_time = 10.0):
         self.device_index = device_index
         self.chunk_rate = chunk_rate
         self.num_channels = num_channels
+        self.autogain_max = autogain_max
+        self.autogain = autogain
+        self.autogain_time = autogain_time
         self.__initstate__()
 
     def __initstate__(self):
         self._audioStream, self._sampleRate = stream_audio(chunk_rate=self.chunk_rate, channels=self.num_channels)
         self._buffer = []
         self._chunk_size = int(self._sampleRate / self.chunk_rate)
+        # increase cur_gain by percentage
+        # we want to get to self.autogain_max in approx. self.autogain_time seconds
+        min_value = 1. / self.autogain_max # the minimum input value we want to bring to 1.0
+        N = self.chunk_rate * self.autogain_time  # N = chunks_per_second * autogain_time
+        # min_value * (perc)^N = 1.0?
+        # perc = root(1.0 / min_value, N) = (1./min_value)**(1/N)
+        self._autogain_perc = (1.0/min_value)**float(1/N)
+        self._cur_gain = 1.0
         super(AudioInput, self).__initstate__()
 
     def numOutputChannels(self):
@@ -113,6 +124,25 @@ class AudioInput(Effect):
     
     def numInputChannels(self):
         return 0
+
+    @staticmethod
+    def getParameterDefinition():
+        definition = {
+            "parameters": {
+                # default, min, max, stepsize
+                "autogain_max": [1.0, 0.0, 50.0, 0.01],
+                "autogain_time": [30.0, 1.0, 100.0, 0.1],
+                "autogain": False
+            }
+        }
+        return definition
+
+    def getParameter(self):
+        definition = self.getParameterDefinition()
+        definition['parameters']['autogain_max'][0] = self.autogain_max
+        definition['parameters']['autogain_time'][0] = self.autogain_time
+        definition['parameters']['autogain'] = self.autogain
+        return definition
 
     def getSampleRate(self):
         return self._sampleRate
@@ -122,7 +152,16 @@ class AudioInput(Effect):
         self._buffer = next(self._audioStream)
 
     def process(self):
+        if self.autogain:
+            # determine max value -> in range 0,1
+            maxVal = np.max(self._buffer)    
+            if maxVal * self._cur_gain > 1:
+                # reset cur_gain to prevent clipping
+                self._cur_gain = 1. / maxVal
+            elif self._cur_gain < self.autogain_max:
+                self._cur_gain = min(self.autogain_max, self._cur_gain * self._autogain_perc)
+            #print("cur_gain: {}, gained value: {}".format(self._cur_gain, self._cur_gain * maxVal))
         for i in range(0,self.num_channels):
             # layout for multiple channel is interleaved:
             # 00 01 .. 0n 10 11 .. 1n
-            self._outputBuffer[i] = self._buffer[i::self.num_channels]
+            self._outputBuffer[i] = self._cur_gain * self._buffer[i::self.num_channels]
