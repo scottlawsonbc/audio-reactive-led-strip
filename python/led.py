72d75730d71753253ea8cf732917a5af774c6f77
BenoitAnastay <45088785+BenoitAnastay@users.noreply.github.com>
2019-12-17 06:11:09 +0100

    Revert "Cleanup and ESP modifications"
    This reverts commit e6631b2d148ed240fa328f05aa9b7065df24edfc.

---
+++ python/led.py
@@ -0,0 +1,164 @@
+from __future__ import print_function
+from __future__ import division
+
+import platform
+import numpy as np
+import config
+
+# ESP8266 uses WiFi communication
+if config.DEVICE == 'esp8266':
+    import socket
+    _sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Raspberry Pi controls the LED strip directly
+elif config.DEVICE == 'pi':
+    import neopixel
+    strip = neopixel.Adafruit_NeoPixel(config.N_PIXELS, config.LED_PIN,
+                                       config.LED_FREQ_HZ, config.LED_DMA,
+                                       config.LED_INVERT, config.BRIGHTNESS)
+    strip.begin()
+elif config.DEVICE == 'blinkstick':
+    from blinkstick import blinkstick
+    import signal
+    import sys
+    #Will turn all leds off when invoked.
+    def signal_handler(signal, frame):
+        all_off = [0]*(config.N_PIXELS*3)
+        stick.set_led_data(0, all_off)
+        sys.exit(0)
+
+    stick = blinkstick.find_first()
+    # Create a listener that turns the leds off when the program terminates
+    signal.signal(signal.SIGTERM, signal_handler)
+    signal.signal(signal.SIGINT, signal_handler)
+
+_gamma = np.load(config.GAMMA_TABLE_PATH)
+"""Gamma lookup table used for nonlinear brightness correction"""
+
+_prev_pixels = np.tile(253, (3, config.N_PIXELS))
+"""Pixel values that were most recently displayed on the LED strip"""
+
+pixels = np.tile(1, (3, config.N_PIXELS))
+"""Pixel values for the LED strip"""
+
+_is_python_2 = int(platform.python_version_tuple()[0]) == 2
+
+def _update_esp8266():
+    """Sends UDP packets to ESP8266 to update LED strip values
+
+    The ESP8266 will receive and decode the packets to determine what values
+    to display on the LED strip. The communication protocol supports LED strips
+    with a maximum of 256 LEDs.
+
+    The packet encoding scheme is:
+        |i|r|g|b|
+    where
+        i (0 to 255): Index of LED to change (zero-based)
+        r (0 to 255): Red value of LED
+        g (0 to 255): Green value of LED
+        b (0 to 255): Blue value of LED
+    """
+    global pixels, _prev_pixels
+    # Truncate values and cast to integer
+    pixels = np.clip(pixels, 0, 255).astype(int)
+    # Optionally apply gamma correc tio
+    p = _gamma[pixels] if config.SOFTWARE_GAMMA_CORRECTION else np.copy(pixels)
+    MAX_PIXELS_PER_PACKET = 126
+    # Pixel indices
+    idx = range(pixels.shape[1])
+    idx = [i for i in idx if not np.array_equal(p[:, i], _prev_pixels[:, i])]
+    n_packets = len(idx) // MAX_PIXELS_PER_PACKET + 1
+    idx = np.array_split(idx, n_packets)
+    for packet_indices in idx:
+        m = '' if _is_python_2 else []
+        for i in packet_indices:
+            if _is_python_2:
+                m += chr(i) + chr(p[0][i]) + chr(p[1][i]) + chr(p[2][i])
+            else:
+                m.append(i)  # Index of pixel to change
+                m.append(p[0][i])  # Pixel red value
+                m.append(p[1][i])  # Pixel green value
+                m.append(p[2][i])  # Pixel blue value
+        m = m if _is_python_2 else bytes(m)
+        _sock.sendto(m, (config.UDP_IP, config.UDP_PORT))
+    _prev_pixels = np.copy(p)
+
+
+def _update_pi():
+    """Writes new LED values to the Raspberry Pi's LED strip
+
+    Raspberry Pi uses the rpi_ws281x to control the LED strip directly.
+    This function updates the LED strip with new values.
+    """
+    global pixels, _prev_pixels
+    # Truncate values and cast to integer
+    pixels = np.clip(pixels, 0, 255).astype(int)
+    # Optional gamma correction
+    p = _gamma[pixels] if config.SOFTWARE_GAMMA_CORRECTION else np.copy(pixels)
+    # Encode 24-bit LED values in 32 bit integers
+    r = np.left_shift(p[0][:].astype(int), 8)
+    g = np.left_shift(p[1][:].astype(int), 16)
+    b = p[2][:].astype(int)
+    rgb = np.bitwise_or(np.bitwise_or(r, g), b)
+    # Update the pixels
+    for i in range(config.N_PIXELS):
+        # Ignore pixels if they haven't changed (saves bandwidth)
+        if np.array_equal(p[:, i], _prev_pixels[:, i]):
+            continue
+        strip._led_data[i] = rgb[i]
+    _prev_pixels = np.copy(p)
+    strip.show()
+
+def _update_blinkstick():
+    """Writes new LED values to the Blinkstick.
+        This function updates the LED strip with new values.
+    """
+    global pixels
+    
+    # Truncate values and cast to integer
+    pixels = np.clip(pixels, 0, 255).astype(int)
+    # Optional gamma correction
+    p = _gamma[pixels] if config.SOFTWARE_GAMMA_CORRECTION else np.copy(pixels)
+    # Read the rgb values
+    r = p[0][:].astype(int)
+    g = p[1][:].astype(int)
+    b = p[2][:].astype(int)
+
+    #create array in which we will store the led states
+    newstrip = [None]*(config.N_PIXELS*3)
+
+    for i in range(config.N_PIXELS):
+        # blinkstick uses GRB format
+        newstrip[i*3] = g[i]
+        newstrip[i*3+1] = r[i]
+        newstrip[i*3+2] = b[i]
+    #send the data to the blinkstick
+    stick.set_led_data(0, newstrip)
+
+
+def update():
+    """Updates the LED strip values"""
+    if config.DEVICE == 'esp8266':
+        _update_esp8266()
+    elif config.DEVICE == 'pi':
+        _update_pi()
+    elif config.DEVICE == 'blinkstick':
+        _update_blinkstick()
+    else:
+        raise ValueError('Invalid device selected')
+
+
+# Execute this file to run a LED strand test
+# If everything is working, you should see a red, green, and blue pixel scroll
+# across the LED strip continously
+if __name__ == '__main__':
+    import time
+    # Turn all pixels off
+    pixels *= 0
+    pixels[0, 0] = 255  # Set 1st pixel red
+    pixels[1, 1] = 255  # Set 2nd pixel green
+    pixels[2, 2] = 255  # Set 3rd pixel blue
+    print('Starting LED strand test')
+    while True:
+        pixels = np.roll(pixels, 1, axis=1)
+        update()
+        time.sleep(.1)
---
+++ python/visualization.py
@@ -0,0 +1,356 @@
+from __future__ import print_function
+from __future__ import division
+import time
+import numpy as np
+from scipy.ndimage.filters import gaussian_filter1d
+import config
+import microphone
+import dsp
+import led
+
+_time_prev = time.time() * 1000.0
+"""The previous time that the frames_per_second() function was called"""
+
+_fps = dsp.ExpFilter(val=config.FPS, alpha_decay=0.2, alpha_rise=0.2)
+"""The low-pass filter used to estimate frames-per-second"""
+
+
+def frames_per_second():
+    """Return the estimated frames per second
+
+    Returns the current estimate for frames-per-second (FPS).
+    FPS is estimated by measured the amount of time that has elapsed since
+    this function was previously called. The FPS estimate is low-pass filtered
+    to reduce noise.
+
+    This function is intended to be called one time for every iteration of
+    the program's main loop.
+
+    Returns
+    -------
+    fps : float
+        Estimated frames-per-second. This value is low-pass filtered
+        to reduce noise.
+    """
+    global _time_prev, _fps
+    time_now = time.time() * 1000.0
+    dt = time_now - _time_prev
+    _time_prev = time_now
+    if dt == 0.0:
+        return _fps.value
+    return _fps.update(1000.0 / dt)
+
+
+def memoize(function):
+    """Provides a decorator for memoizing functions"""
+    from functools import wraps
+    memo = {}
+
+    @wraps(function)
+    def wrapper(*args):
+        if args in memo:
+            return memo[args]
+        else:
+            rv = function(*args)
+            memo[args] = rv
+            return rv
+    return wrapper
+
+
+@memoize
+def _normalized_linspace(size):
+    return np.linspace(0, 1, size)
+
+
+def interpolate(y, new_length):
+    """Intelligently resizes the array by linearly interpolating the values
+
+    Parameters
+    ----------
+    y : np.array
+        Array that should be resized
+
+    new_length : int
+        The length of the new interpolated array
+
+    Returns
+    -------
+    z : np.array
+        New array with length of new_length that contains the interpolated
+        values of y.
+    """
+    if len(y) == new_length:
+        return y
+    x_old = _normalized_linspace(len(y))
+    x_new = _normalized_linspace(new_length)
+    z = np.interp(x_new, x_old, y)
+    return z
+
+
+r_filt = dsp.ExpFilter(np.tile(0.01, config.N_PIXELS // 2),
+                       alpha_decay=0.2, alpha_rise=0.99)
+g_filt = dsp.ExpFilter(np.tile(0.01, config.N_PIXELS // 2),
+                       alpha_decay=0.05, alpha_rise=0.3)
+b_filt = dsp.ExpFilter(np.tile(0.01, config.N_PIXELS // 2),
+                       alpha_decay=0.1, alpha_rise=0.5)
+common_mode = dsp.ExpFilter(np.tile(0.01, config.N_PIXELS // 2),
+                       alpha_decay=0.99, alpha_rise=0.01)
+p_filt = dsp.ExpFilter(np.tile(1, (3, config.N_PIXELS // 2)),
+                       alpha_decay=0.1, alpha_rise=0.99)
+p = np.tile(1.0, (3, config.N_PIXELS // 2))
+gain = dsp.ExpFilter(np.tile(0.01, config.N_FFT_BINS),
+                     alpha_decay=0.001, alpha_rise=0.99)
+
+
+def visualize_scroll(y):
+    """Effect that originates in the center and scrolls outwards"""
+    global p
+    y = y**2.0
+    gain.update(y)
+    y /= gain.value
+    y *= 255.0
+    r = int(np.max(y[:len(y) // 3]))
+    g = int(np.max(y[len(y) // 3: 2 * len(y) // 3]))
+    b = int(np.max(y[2 * len(y) // 3:]))
+    # Scrolling effect window
+    p[:, 1:] = p[:, :-1]
+    p *= 0.98
+    p = gaussian_filter1d(p, sigma=0.2)
+    # Create new color originating at the center
+    p[0, 0] = r
+    p[1, 0] = g
+    p[2, 0] = b
+    # Update the LED strip
+    return np.concatenate((p[:, ::-1], p), axis=1)
+
+
+def visualize_energy(y):
+    """Effect that expands from the center with increasing sound energy"""
+    global p
+    y = np.copy(y)
+    gain.update(y)
+    y /= gain.value
+    # Scale by the width of the LED strip
+    y *= float((config.N_PIXELS // 2) - 1)
+    # Map color channels according to energy in the different freq bands
+    scale = 0.9
+    r = int(np.mean(y[:len(y) // 3]**scale))
+    g = int(np.mean(y[len(y) // 3: 2 * len(y) // 3]**scale))
+    b = int(np.mean(y[2 * len(y) // 3:]**scale))
+    # Assign color to different frequency regions
+    p[0, :r] = 255.0
+    p[0, r:] = 0.0
+    p[1, :g] = 255.0
+    p[1, g:] = 0.0
+    p[2, :b] = 255.0
+    p[2, b:] = 0.0
+    p_filt.update(p)
+    p = np.round(p_filt.value)
+    # Apply substantial blur to smooth the edges
+    p[0, :] = gaussian_filter1d(p[0, :], sigma=4.0)
+    p[1, :] = gaussian_filter1d(p[1, :], sigma=4.0)
+    p[2, :] = gaussian_filter1d(p[2, :], sigma=4.0)
+    # Set the new pixel value
+    return np.concatenate((p[:, ::-1], p), axis=1)
+
+
+_prev_spectrum = np.tile(0.01, config.N_PIXELS // 2)
+
+
+def visualize_spectrum(y):
+    """Effect that maps the Mel filterbank frequencies onto the LED strip"""
+    global _prev_spectrum
+    y = np.copy(interpolate(y, config.N_PIXELS // 2))
+    common_mode.update(y)
+    diff = y - _prev_spectrum
+    _prev_spectrum = np.copy(y)
+    # Color channel mappings
+    r = r_filt.update(y - common_mode.value)
+    g = np.abs(diff)
+    b = b_filt.update(np.copy(y))
+    # Mirror the color channels for symmetric output
+    r = np.concatenate((r[::-1], r))
+    g = np.concatenate((g[::-1], g))
+    b = np.concatenate((b[::-1], b))
+    output = np.array([r, g,b]) * 255
+    return output
+
+
+fft_plot_filter = dsp.ExpFilter(np.tile(1e-1, config.N_FFT_BINS),
+                         alpha_decay=0.5, alpha_rise=0.99)
+mel_gain = dsp.ExpFilter(np.tile(1e-1, config.N_FFT_BINS),
+                         alpha_decay=0.01, alpha_rise=0.99)
+mel_smoothing = dsp.ExpFilter(np.tile(1e-1, config.N_FFT_BINS),
+                         alpha_decay=0.5, alpha_rise=0.99)
+volume = dsp.ExpFilter(config.MIN_VOLUME_THRESHOLD,
+                       alpha_decay=0.02, alpha_rise=0.02)
+fft_window = np.hamming(int(config.MIC_RATE / config.FPS) * config.N_ROLLING_HISTORY)
+prev_fps_update = time.time()
+
+
+def microphone_update(audio_samples):
+    global y_roll, prev_rms, prev_exp, prev_fps_update
+    # Normalize samples between 0 and 1
+    y = audio_samples / 2.0**15
+    # Construct a rolling window of audio samples
+    y_roll[:-1] = y_roll[1:]
+    y_roll[-1, :] = np.copy(y)
+    y_data = np.concatenate(y_roll, axis=0).astype(np.float32)
+    
+    vol = np.max(np.abs(y_data))
+    if vol < config.MIN_VOLUME_THRESHOLD:
+        print('No audio input. Volume below threshold. Volume:', vol)
+        led.pixels = np.tile(0, (3, config.N_PIXELS))
+        led.update()
+    else:
+        # Transform audio input into the frequency domain
+        N = len(y_data)
+        N_zeros = 2**int(np.ceil(np.log2(N))) - N
+        # Pad with zeros until the next power of two
+        y_data *= fft_window
+        y_padded = np.pad(y_data, (0, N_zeros), mode='constant')
+        YS = np.abs(np.fft.rfft(y_padded)[:N // 2])
+        # Construct a Mel filterbank from the FFT data
+        mel = np.atleast_2d(YS).T * dsp.mel_y.T
+        # Scale data to values more suitable for visualization
+        # mel = np.sum(mel, axis=0)
+        mel = np.sum(mel, axis=0)
+        mel = mel**2.0
+        # Gain normalization
+        mel_gain.update(np.max(gaussian_filter1d(mel, sigma=1.0)))
+        mel /= mel_gain.value
+        mel = mel_smoothing.update(mel)
+        # Map filterbank output onto LED strip
+        output = visualization_effect(mel)
+        led.pixels = output
+        led.update()
+        if config.USE_GUI:
+            # Plot filterbank output
+            x = np.linspace(config.MIN_FREQUENCY, config.MAX_FREQUENCY, len(mel))
+            mel_curve.setData(x=x, y=fft_plot_filter.update(mel))
+            # Plot the color channels
+            r_curve.setData(y=led.pixels[0])
+            g_curve.setData(y=led.pixels[1])
+            b_curve.setData(y=led.pixels[2])
+    if config.USE_GUI:
+        app.processEvents()
+    
+    if config.DISPLAY_FPS:
+        fps = frames_per_second()
+        if time.time() - 0.5 > prev_fps_update:
+            prev_fps_update = time.time()
+            print('FPS {:.0f} / {:.0f}'.format(fps, config.FPS))
+
+
+# Number of audio samples to read every time frame
+samples_per_frame = int(config.MIC_RATE / config.FPS)
+
+# Array containing the rolling audio sample window
+y_roll = np.random.rand(config.N_ROLLING_HISTORY, samples_per_frame) / 1e16
+
+visualization_effect = visualize_spectrum
+"""Visualization effect to display on the LED strip"""
+
+
+if __name__ == '__main__':
+    if config.USE_GUI:
+        import pyqtgraph as pg
+        from pyqtgraph.Qt import QtGui, QtCore
+        # Create GUI window
+        app = QtGui.QApplication([])
+        view = pg.GraphicsView()
+        layout = pg.GraphicsLayout(border=(100,100,100))
+        view.setCentralItem(layout)
+        view.show()
+        view.setWindowTitle('Visualization')
+        view.resize(800,600)
+        # Mel filterbank plot
+        fft_plot = layout.addPlot(title='Filterbank Output', colspan=3)
+        fft_plot.setRange(yRange=[-0.1, 1.2])
+        fft_plot.disableAutoRange(axis=pg.ViewBox.YAxis)
+        x_data = np.array(range(1, config.N_FFT_BINS + 1))
+        mel_curve = pg.PlotCurveItem()
+        mel_curve.setData(x=x_data, y=x_data*0)
+        fft_plot.addItem(mel_curve)
+        # Visualization plot
+        layout.nextRow()
+        led_plot = layout.addPlot(title='Visualization Output', colspan=3)
+        led_plot.setRange(yRange=[-5, 260])
+        led_plot.disableAutoRange(axis=pg.ViewBox.YAxis)
+        # Pen for each of the color channel curves
+        r_pen = pg.mkPen((255, 30, 30, 200), width=4)
+        g_pen = pg.mkPen((30, 255, 30, 200), width=4)
+        b_pen = pg.mkPen((30, 30, 255, 200), width=4)
+        # Color channel curves
+        r_curve = pg.PlotCurveItem(pen=r_pen)
+        g_curve = pg.PlotCurveItem(pen=g_pen)
+        b_curve = pg.PlotCurveItem(pen=b_pen)
+        # Define x data
+        x_data = np.array(range(1, config.N_PIXELS + 1))
+        r_curve.setData(x=x_data, y=x_data*0)
+        g_curve.setData(x=x_data, y=x_data*0)
+        b_curve.setData(x=x_data, y=x_data*0)
+        # Add curves to plot
+        led_plot.addItem(r_curve)
+        led_plot.addItem(g_curve)
+        led_plot.addItem(b_curve)
+        # Frequency range label
+        freq_label = pg.LabelItem('')
+        # Frequency slider
+        def freq_slider_change(tick):
+            minf = freq_slider.tickValue(0)**2.0 * (config.MIC_RATE / 2.0)
+            maxf = freq_slider.tickValue(1)**2.0 * (config.MIC_RATE / 2.0)
+            t = 'Frequency range: {:.0f} - {:.0f} Hz'.format(minf, maxf)
+            freq_label.setText(t)
+            config.MIN_FREQUENCY = minf
+            config.MAX_FREQUENCY = maxf
+            dsp.create_mel_bank()
+        freq_slider = pg.TickSliderItem(orientation='bottom', allowAdd=False)
+        freq_slider.addTick((config.MIN_FREQUENCY / (config.MIC_RATE / 2.0))**0.5)
+        freq_slider.addTick((config.MAX_FREQUENCY / (config.MIC_RATE / 2.0))**0.5)
+        freq_slider.tickMoveFinished = freq_slider_change
+        freq_label.setText('Frequency range: {} - {} Hz'.format(
+            config.MIN_FREQUENCY,
+            config.MAX_FREQUENCY))
+        # Effect selection
+        active_color = '#16dbeb'
+        inactive_color = '#FFFFFF'
+        def energy_click(x):
+            global visualization_effect
+            visualization_effect = visualize_energy
+            energy_label.setText('Energy', color=active_color)
+            scroll_label.setText('Scroll', color=inactive_color)
+            spectrum_label.setText('Spectrum', color=inactive_color)
+        def scroll_click(x):
+            global visualization_effect
+            visualization_effect = visualize_scroll
+            energy_label.setText('Energy', color=inactive_color)
+            scroll_label.setText('Scroll', color=active_color)
+            spectrum_label.setText('Spectrum', color=inactive_color)
+        def spectrum_click(x):
+            global visualization_effect
+            visualization_effect = visualize_spectrum
+            energy_label.setText('Energy', color=inactive_color)
+            scroll_label.setText('Scroll', color=inactive_color)
+            spectrum_label.setText('Spectrum', color=active_color)
+        # Create effect "buttons" (labels with click event)
+        energy_label = pg.LabelItem('Energy')
+        scroll_label = pg.LabelItem('Scroll')
+        spectrum_label = pg.LabelItem('Spectrum')
+        energy_label.mousePressEvent = energy_click
+        scroll_label.mousePressEvent = scroll_click
+        spectrum_label.mousePressEvent = spectrum_click
+        energy_click(0)
+        # Layout
+        layout.nextRow()
+        layout.addItem(freq_label, colspan=3)
+        layout.nextRow()
+        layout.addItem(freq_slider, colspan=3)
+        layout.nextRow()
+        layout.addItem(energy_label)
+        layout.addItem(scroll_label)
+        layout.addItem(spectrum_label)
+    # Initialize LEDs
+    led.update()
+    # Start listening to live audio stream
+    microphone.start_stream(microphone_update)
