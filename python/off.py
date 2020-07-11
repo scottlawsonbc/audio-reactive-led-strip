1e88546befe0ff96de37503e585163b81e060067
elyobelyob <nick@elyob.com>
2020-07-11 23:27:29 +0100

    Update off.py
    I love your rebuild. I have visions ... fancy a chat?
    Use the config.

--- python/off.py
+++ python/off.py
@@ -3,15 +3,17 @@
 import time
 from rpi_ws281x import *
 import argparse
+import config
 
 # LED strip configuration:
-LED_COUNT = 144  # Number of LED pixels.
-LED_PIN = 18  # GPIO pin connected to the pixels (18 uses PWM!).
-# LED_PIN = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA = 10  # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
-LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
+LED_COUNT = config.N_PIXELS # Number of LED pixels.
+LED_PIN = config.LED_PIN  # GPIO pin connected to the pixels (18 uses PWM!).
+LED_FREQ_HZ = config.LED_FREQ_HZ   # LED signal frequency in hertz (usually 800khz)
+LED_DMA = config.LED_DMA  # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = config.BRIGHTNESS  # Set to 0 for darkest and 255 for brightest
+LED_INVERT = config.LED_INVERT  # True to invert the signal (when using NPN transistor level shift)
+
+# not in config
 LED_CHANNEL = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 
