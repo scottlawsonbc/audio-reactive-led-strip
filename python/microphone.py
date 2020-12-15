5f1c9837fe58b06df7a69756a74ef7f7dfd1899e
Nazmus Nasir <nazmus.nasir@gmail.com>
2020-12-15 02:31:12 -0500

    Restoring original script

--- python/microphone.py
+++ python/microphone.py
@@ -2,39 +2,24 @@ import time
 import numpy as np
 import pyaudio
 import config
-from pydub import AudioSegment
-from pydub.utils import make_chunks
+
 
 def start_stream(callback):
-    sound = AudioSegment.from_file("/home/pi/Stars_Align.mp3")
-
     p = pyaudio.PyAudio()
     frames_per_buffer = int(config.MIC_RATE / config.FPS)
     stream = p.open(format=pyaudio.paInt16,
-                    channels=sound.channels,
+                    channels=1,
                     rate=config.MIC_RATE,
-                    # input=True,
-                    output=True,
+                    input=True,
                     frames_per_buffer=frames_per_buffer)
     overflows = 0
     prev_ovf_time = time.time()
     while True:
         try:
-            # y = np.fromstring(stream.read(frames_per_buffer, exception_on_overflow=False), dtype=np.int16)
-            # y = y.astype(np.float32)
-            # stream.read(stream.get_read_available(), exception_on_overflow=False)
-            # callback(y)
-            start = 0
-            length = sound.duration_seconds
-            volume = 100.0
-            playchunk = sound[start * 1000.0:(start + length) * 1000.0] - (60 - (60 * (volume / 100.0)))
-            millisecondchunk = 50 / 1000.0
-
-            for chunks in make_chunks(playchunk, millisecondchunk * 1000):
-                stream.write(chunks._data)
-                y = np.frombuffer(chunks._data, dtype=np.int16)
-                y = y.astype(np.float32)
-                callback(y)
+            y = np.fromstring(stream.read(frames_per_buffer, exception_on_overflow=False), dtype=np.int16)
+            y = y.astype(np.float32)
+            stream.read(stream.get_read_available(), exception_on_overflow=False)
+            callback(y)
         except IOError:
             overflows += 1
             if time.time() > prev_ovf_time + 1:
