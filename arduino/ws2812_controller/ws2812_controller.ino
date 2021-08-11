3096eb1ae42bdd2b9e1ce9ed3b7031894731ebf8
Joey Babcock <joey_babcock@yahoo.com>
2021-08-11 13:29:52 -0700

    Combined ESP32 and ESP8266 thanks to @quotschmacher in #342

--- arduino/ws2812_controller/ws2812_controller.ino
+++ arduino/ws2812_controller/ws2812_controller.ino
@@ -1,14 +1,16 @@
-  
 /*
-* This example works for ESP8266 and uses the NeoPixelBus library instead of the one bundle
-* Sketch contributed to by Joey Babcock - https://joeybabcock.me/blog/
+* This example works for ESP8266 & ESP32 and uses the NeoPixelBus library instead of the one bundle
+* Sketch written by Joey Babcock - https://joeybabcock.me/blog/, and Scott Lawson (Below) 
 * Codebase created by ScottLawsonBC - https://github.com/scottlawsonbc
 */
 
-#include <Arduino.h>
+#if defined(ESP8266)
 #include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
-#include <NeoPixelBus.h>
+#elif defined(ESP32)
+#include <WiFi.h>
+#else
+#error "This is not a ESP8266 or ESP32!"
+#endif
 
 // Set to the number of LEDs in your LED strip
 #define NUM_LEDS 60
@@ -26,41 +28,41 @@ const char* password = "YOUR_WIFI_PASSWORD";
 unsigned int localPort = 7777;
 char packetBuffer[BUFFER_LEN];
 
-// LED strip
-NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> ledstrip(NUM_LEDS, PixelPin);
+uint8_t N = 0;
 
 WiFiUDP port;
-
 // Network information
-// IP must match the IP in config.py in python folder
+// IP must match the IP in config.py
 IPAddress ip(192, 168, 0, 150);
 // Set gateway to your router's gateway
 IPAddress gateway(192, 168, 0, 1);
 IPAddress subnet(255, 255, 255, 0);
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> ledstrip(NUM_LEDS, PixelPin);
 
 void setup() {
     Serial.begin(115200);
-    
+    WiFi.mode(WIFI_STA);
     WiFi.config(ip, gateway, subnet);
     WiFi.begin(ssid, password);
-    WiFi.mode(WIFI_STA);
     Serial.println("");
     // Connect to wifi and print the IP address over serial
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
+    
     Serial.println("");
     Serial.print("Connected to ");
     Serial.println(ssid);
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
+    
     port.begin(localPort);
+    
     ledstrip.Begin();//Begin output
     ledstrip.Show();//Clear the strip for use
 }
 
-uint8_t N = 0;
 #if PRINT_FPS
     uint16_t fpsCounter = 0;
     uint32_t secondTimer = 0;
@@ -75,8 +77,8 @@ void loop() {
         for(int i = 0; i < len; i+=4) {
             packetBuffer[len] = 0;
             N = packetBuffer[i];
-            RgbColor pixel((uint8_t)packetBuffer[i+1], (uint8_t)packetBuffer[i+2], (uint8_t)packetBuffer[i+3]);
-            ledstrip.SetPixelColor(N, pixel);
+            RgbColor pixel((uint8_t)packetBuffer[i+1], (uint8_t)packetBuffer[i+2], (uint8_t)packetBuffer[i+3]);//color
+            ledstrip.SetPixelColor(N, pixel);//N is the pixel number
         } 
         ledstrip.Show();
         #if PRINT_FPS
